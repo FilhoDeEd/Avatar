ef8a5eb45063128a46d0ec0dfad6806d1a37ec86
Edson Rodrigues <edson.er.rodrigues@gmail.com>
2022-11-16 15:09:43 -0300

    feat(listaDinamica): Modulo para lidar com listas dinâmicas encadeadas

---
+++ source/listaDinamica.c
@@ -0,0 +1,126 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "listaDinamica.h"
+
+typedef struct unidade
+{
+    Ponto dado;
+    struct unidade* prox;
+}Unidade;
+
+typedef struct lista
+{
+    Unidade* inicio;
+    Unidade* final;
+    int qtd;
+}Lista;
+
+Ponto init_ponto(double x, double y, int ID)
+{
+    Ponto p;
+
+    p.x = x;
+    p.y = y;
+    p.ID = ID;
+
+    return p;
+}
+
+Unidade* cria_unidade(Ponto p)
+{
+    Unidade* un = (Unidade*) malloc(sizeof(Unidade));
+    if(un != NULL)
+    {
+        un->dado = p;
+        un->prox = NULL;
+    }
+
+    return un;
+}
+
+void libera_unidade(Unidade* un)
+{
+    if(un != NULL)
+    {
+        free(un);
+    }
+}
+
+Lista* cria_lista()
+{
+    Lista* li = (Lista*) malloc(sizeof(Lista));
+    if(li != NULL)
+    {
+        li->inicio = NULL;
+        li->final = NULL;
+        li->qtd = 0;
+    }
+
+    return li;
+}
+
+void libera_lista(Lista* li)
+{
+    if(li != NULL)
+    {
+        Unidade* no;
+
+        while((li->inicio) != NULL)
+        {
+            no = li->inicio;
+            li->inicio = li->inicio->prox;
+            free(no);
+        }
+
+        free(li);
+    }
+}
+
+int tamanho_lista(Lista* li)
+{
+    if(li == NULL) return -1;
+
+    return li->qtd;
+}
+
+int lista_vazia(Lista* li)
+{
+    if(li == NULL) return -1;
+
+    if(li->inicio == NULL) return 1;
+
+    return 0;
+}
+
+int insere_uni_lista_final(Lista* li, Unidade* no)
+{
+    if(li == NULL) return -1;
+
+    if(li->inicio == NULL) li->inicio = no;
+    else li->final->prox = no;
+
+    li->final = no;
+    (li->qtd)++;
+    return 1;
+}
+
+int remove_uni_lista(Lista* li, int ID)
+{
+    if(li == NULL) return -1;
+
+    Unidade *anterior, *no = li->inicio;
+
+    while(no != NULL && no->dado.ID != ID)
+    {
+        anterior = no;
+        no = no->prox;
+    }
+
+    if(no == NULL) return 0;
+
+    if(no == li->inicio) li->inicio = no->prox;
+    else anterior->prox = no->prox;
+    
+    (li->qtd)--;
+    return 1;
+}
---
+++ source/listaDinamica.h
@@ -0,0 +1,43 @@
+#ifndef LISTADINAMICA_H
+#define LISTADINAMICA_H
+
+typedef struct ponto
+{
+    int ID;
+    double x;
+    double y;
+}Ponto;
+
+typedef struct unidade Unidade;
+typedef struct lista Lista;
+
+//Inicializa um ponto
+Ponto init_ponto(double x, double y, int ID);
+
+//Cria uma unidade da lista
+Unidade* cria_unidade(Ponto p);
+
+//Exclui uma unidade (remova da lista antes de excluir)
+void libera_unidade(Unidade* un);
+
+//Cria a lista
+Lista* cria_lista();
+
+//Exclui a lista
+void libera_lista(Lista* li);
+
+//Retorna o número de elementos na lista. Caso a lista seja inválida, retorna -1
+int tamanho_lista(Lista* li);
+
+//Retorna se a lista está vazia ou não. Caso a lista seja inválida, retorna -1
+int lista_vazia(Lista* li);
+
+//Insere um elemento ao final da lista e retorna 1 se a remoção ocorreu com êxito.
+//Caso a lista seja inválida, retorna -1
+int insere_uni_lista_final(Lista* li, Unidade* no);
+
+//Remove um elemento da lista com base no ID e retorna 1 se a remoção ocorreu com 
+//êxito ou 0 se falhou. Caso a lista seja inválida, retorna -1
+int remove_uni_lista(Lista* li, int ID);
+
+#endif /* LISTADINAMICA_H */
