0b90d74e6c7d783988bcbeca15a142d8213ae879
Edson Rodrigues <edson.er.rodrigues@gmail.com>
2022-11-16 15:12:52 -0300

    feat(main.c): Criei as listas que vão armazenar os pontos

--- source/main.c
+++ source/main.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "elemento.h"
+#include "listaDinamica.h"
+
+#define NE 3 //Aumente em 1 para cada erro adicionado no enum
+enum erros {EFOGO=-NE,EAGUA,EGRAMA};
+
+//Listas para armazenar a posição de todos os pontos
+Lista* ListaFogo;
+Lista* ListaAgua;
+Lista* ListaGrama;
 
 int main()
-{
-    teste();
+{  
+    ListaFogo = cria_lista();
+    if(ListaFogo == NULL)
+    {
+        printf("Erro ao criar 'ListaFogo'.\n");
+        exit(EFOGO);
+    }
+
+    ListaAgua = cria_lista();
+    if(ListaAgua == NULL)
+    {
+        printf("Erro ao criar 'ListaAgua'.\n");
+        exit(EAGUA);
+    }
+
+    ListaGrama = cria_lista();
+    if(ListaGrama == NULL)
+    {
+        printf("Erro ao criar 'listaGrama'.\n");
+        exit(EGRAMA);
+    }
+
     return 0;
 }
