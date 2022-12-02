5773245a1647575c1464ef50ff451ca86f9221a6
Edson Rodrigues <edson.er.rodrigues@gmail.com>
2022-12-02 20:57:04 -0300

    feat(elemento.h): Novas variáveis para controlar acesso ao arquivo e execução das threads

--- source/elemento.h
+++ source/elemento.h
@@ -8,10 +8,23 @@
 #define AGUA 'a'
 #define GRAMA 'g'
 
+//Constantes para dimensões da área de simulação
+#define LARGURA_TELA 1000
+#define ALTURA_TELA 800
+
+//Numero de threads
+#define NUMTHREADS 30
+
 //Listas para armazenar a posição de todos os pontos
 Lista* ListaFogo;
 Lista* ListaAgua;
 Lista* ListaGrama;
+
+//Declaração das threads
+pthread_t threadsElementos[NUMTHREADS];
+
+//Declaração do arquivo de rastro
+FILE* arqRastro;
 
 //Controlar leitores e escritores das listas
 pthread_mutex_t mutexTemEscritor;
@@ -22,5 +35,20 @@ int numLeitores;
 
 //Controlar acesso às listas
 pthread_mutex_t mutexListas;
+
+//Controlar acesso ao arquivo
+pthread_mutex_t mutexArquivo;
+
+//Controlar tempo de execução das threads principais
+pthread_mutex_t mutexLargada;
+pthread_cond_t condLargada;
+int largada;
+int pararSimulacao;
+
+//Rotina principal da simulação
+void* thrElemento(void* argsThrElemento);
+
+//Rotina para monitorar e controlar a simulação
+void* thrMonitora(void* args);
 
 #endif /* ELEMENTO_H */
