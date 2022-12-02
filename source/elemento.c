d980ad00ccd31179167439e13e9a6053b5ed26b2
Edson Rodrigues <edson.er.rodrigues@gmail.com>
2022-12-02 18:54:41 -0300

    feat(elemento.c): Cálculo do movimento implementado; fix(elemento.c): Havia um bug em detectar colisão

--- source/elemento.c
+++ source/elemento.c
@@ -3,7 +3,13 @@
 #include <pthread.h>
 #include "elemento.h"
 #include "fisicaDinamica.h"
+
+//Algumas constantes do movimento
 #define RAIO 10.0
+#define MASSA 5.0
+#define ATRACAO 1.0
+#define REPULSAO -0.1
+#define dt 0.042
 
 typedef struct argumentos
 {
@@ -66,83 +72,189 @@ void trocaUnidadeListas(Unidade *uni)
 
 void* thrElemento(void* endeArgs)
 {
+    //Argumentos
     Args* args = (Args*) endeArgs;
-
     char corInicial = args->corInicial;
     Unidade* respectivaUnidade = args->respectivaUnidade;
-    Elemento elemento;
 
-    ///Iniciando elemento
+    //Declarando e inicializando elemento
+    Elemento elemento;
     trocaElemento(corInicial,&elemento);
     elemento.respectivaUnidade = respectivaUnidade;
-    ///fim
 
-    ///Verificando se deve trocar de elemento
-        //Variáveis necessárias
-    Unidade* unidadeElementoFracoContra = elemento.elementoFracoContra->inicio;
+    //Variáveis locais
+    Unidade* unidadeElementoFracoContra;
+    Unidade* unidadeElementoForteContra;
     double posicaoAtualElemento[2];
+        posicaoAtualElemento[0] = elemento.respectivaUnidade->dado.x;
+        posicaoAtualElemento[1] = elemento.respectivaUnidade->dado.y;
+    double velocidadeAtualElemento[2];
+        velocidadeAtualElemento[0] = 0.0;
+        velocidadeAtualElemento[1] = 0.0;
     double posicaoOutrosElementos[2];
     double dist;
-    posicaoAtualElemento[0] = elemento.respectivaUnidade->dado.x;
-    posicaoAtualElemento[1] = elemento.respectivaUnidade->dado.y;
+    double dist_horizon;
+    double dist_verti;
+    double forca[2];
+    double forcaResultante[2];
+    double aceleracao[2];
 
-        //Se houver escritor, espera
-    pthread_mutex_lock(&mutexTemEscritor);
-    while(temEscritor)
+    //-----------------------------------------------------------------------------------//
+
+    //Repita até ser sinalizado para parar
+    while(1)
     {
-        pthread_cond_wait(&condEscritor,&mutexTemEscritor);
-    }
-    pthread_mutex_unlock(&mutexTemEscritor);
+        //----------------------Verificar colisão entre elementos----------------------//
 
-        //Há mais um leitor. Se for o primeiro, bloqueie as listas
-    pthread_mutex_lock(&mutexNumLeitores);
-    numLeitores++;
-    if(numLeitores == 1) pthread_mutex_lock(&mutexListas);
-    pthread_mutex_unlock(&mutexNumLeitores);
-
-        //Lendo a lista e utilizando seus dados
-    while(unidadeElementoFracoContra != NULL)
-    {
-        posicaoOutrosElementos[0] = unidadeElementoFracoContra->dado.x;
-        posicaoOutrosElementos[1] = unidadeElementoFracoContra->dado.y;
-
-        dist = calcula_distancia(posicaoAtualElemento,posicaoOutrosElementos);
-
-        //Condição de colisão entre dois círculos
-        if(dist < 2*RAIO)
+            //Se houver escritor, espere
+        pthread_mutex_lock(&mutexTemEscritor);
+        while(temEscritor)
         {
-            //Sinalizar que há um novo escritor
-            pthread_mutex_lock(&mutexTemEscritor);
-            temEscritor++;
-            pthread_mutex_unlock(&mutexTemEscritor);
+            pthread_cond_wait(&condEscritor,&mutexTemEscritor);
+        }
+        pthread_mutex_unlock(&mutexTemEscritor);
 
-            //Hora de escrever. É necessário acesso exclusivo às listas
-            pthread_mutex_lock(&mutexListas);
-            trocaUnidadeListas(elemento.respectivaUnidade);
-            trocaElemento(unidadeElementoFracoContra->dado.cor,&elemento);
-            pthread_mutex_unlock(&mutexListas);
+            //Há mais um leitor. Se for o primeiro, bloqueie as listas
+        pthread_mutex_lock(&mutexNumLeitores);
+        numLeitores++;
+        if(numLeitores == 1) pthread_mutex_lock(&mutexListas);
+        pthread_mutex_unlock(&mutexNumLeitores);
 
-            //Sinalizar que deixou de ser escritor
-            pthread_mutex_lock(&mutexTemEscritor);
-            temEscritor--;
-            pthread_mutex_unlock(&mutexTemEscritor);
+            //Lendo a lista e utilizando seus dados
+        unidadeElementoFracoContra = elemento.elementoFracoContra->inicio;
+        while(unidadeElementoFracoContra != NULL)
+        {
+            posicaoOutrosElementos[0] = unidadeElementoFracoContra->dado.x;
+            posicaoOutrosElementos[1] = unidadeElementoFracoContra->dado.y;
 
-            //Acordar os leitores em espera
-            pthread_cond_signal(&condEscritor);
+            dist = calcula_distancia(posicaoAtualElemento,posicaoOutrosElementos);
 
-            //Pode sair. Já foi detectado uma colisão
-            break;
+            //Condição de colisão entre dois círculos
+            if(dist < 2*RAIO)
+            {   
+                //Há menos um leitor. Se for o último, libere as listas
+                pthread_mutex_lock(&mutexNumLeitores);
+                numLeitores--;
+                if(numLeitores == 0) pthread_mutex_unlock(&mutexListas);
+                pthread_mutex_unlock(&mutexNumLeitores);
+
+                //Sinalizar que há um novo escritor
+                pthread_mutex_lock(&mutexTemEscritor);
+                temEscritor++;
+                pthread_mutex_unlock(&mutexTemEscritor);
+
+                //Hora de escrever. É necessário acesso exclusivo às listas
+                pthread_mutex_lock(&mutexListas);
+                trocaUnidadeListas(elemento.respectivaUnidade);
+                trocaElemento(unidadeElementoFracoContra->dado.cor,&elemento);
+                pthread_mutex_unlock(&mutexListas);
+
+                //Sinalizar que deixou de ser escritor
+                pthread_mutex_lock(&mutexTemEscritor);
+                temEscritor--;
+                pthread_mutex_unlock(&mutexTemEscritor);
+
+                //Acordar os leitores em espera
+                pthread_cond_signal(&condEscritor);
+
+                //Pode sair. Já foi detectado uma colisão
+                break;
+            }
+
+            //Vericar o próximo da lista
+            unidadeElementoFracoContra = unidadeElementoFracoContra->prox;
         }
 
-        //Vericar o próximo da lista
-        unidadeElementoFracoContra = unidadeElementoFracoContra->prox;
-    }
-        //Há menos um leitor. Se for o último, libere as listas
-    pthread_mutex_lock(&mutexNumLeitores);
-    numLeitores--;
-    if(numLeitores == 0) pthread_mutex_unlock(&mutexListas);
-    pthread_mutex_unlock(&mutexNumLeitores);
-    ///fim
+        if(unidadeElementoFracoContra == NULL)
+        {
+            //Há menos um leitor. Se for o último, libere as listas
+            pthread_mutex_lock(&mutexNumLeitores);
+            numLeitores--;
+            if(numLeitores == 0) pthread_mutex_unlock(&mutexListas);
+            pthread_mutex_unlock(&mutexNumLeitores);
+        }
 
-    ///Calcular movimento do elemento 
+        //----------------------Calcular movimento do elemento----------------------//
+        
+            //Inicializando variáveis para calcular o movimento do elemento
+        forcaResultante[0] = 0.0;
+        forcaResultante[1] = 0.0; 
+
+            //Se houver escritor, espere
+        pthread_mutex_lock(&mutexTemEscritor);
+        while(temEscritor)
+        {
+            pthread_cond_wait(&condEscritor,&mutexTemEscritor);
+        }
+        pthread_mutex_unlock(&mutexTemEscritor);
+
+            //Há mais um leitor. Se for o primeiro, bloqueie as listas
+        pthread_mutex_lock(&mutexNumLeitores);
+        numLeitores++;
+        if(numLeitores == 1) pthread_mutex_lock(&mutexListas);
+        pthread_mutex_unlock(&mutexNumLeitores);
+
+            //Lendo a listaForteContra e utilizando seus dados
+        unidadeElementoForteContra = elemento.elementoForteContra->inicio;
+        while(unidadeElementoForteContra != NULL)
+        {
+            //Pegando posição de uma partícula que o elemento é forte contra
+            posicaoOutrosElementos[0] = unidadeElementoForteContra->dado.x;
+            posicaoOutrosElementos[1] = unidadeElementoForteContra->dado.y;
+
+            //Calculando distâncias
+            dist_horizon = posicaoOutrosElementos[0] - posicaoAtualElemento[0];
+            dist_verti = posicaoOutrosElementos[1] - posicaoAtualElemento[1];
+            dist = calcula_distancia(posicaoAtualElemento,posicaoOutrosElementos);
+            
+            //Calculando força de atração resultante
+            calcula_forca(forca,ATRACAO,dist,dist_horizon,dist_verti);
+            forcaResultante[0] += forca[0];
+            forcaResultante[1] += forca[1];
+
+            //Calcular com próximo da lista
+            unidadeElementoForteContra = unidadeElementoForteContra->prox;
+        }
+
+            //Lendo a listaFracoContra e utilizando seus dados
+        unidadeElementoFracoContra = elemento.elementoFracoContra->inicio;
+        while(unidadeElementoFracoContra != NULL)
+        {
+            //Pegando posição de uma partícula que o elemento é forte contra
+            posicaoOutrosElementos[0] = unidadeElementoFracoContra->dado.x;
+            posicaoOutrosElementos[1] = unidadeElementoFracoContra->dado.y;
+
+            //Calculando distâncias
+            dist_horizon = posicaoOutrosElementos[0] - posicaoAtualElemento[0];
+            dist_verti = posicaoOutrosElementos[1] - posicaoAtualElemento[1];
+            dist = calcula_distancia(posicaoAtualElemento,posicaoOutrosElementos);
+            
+            //Calculando força resultante final
+            calcula_forca(forca,REPULSAO,dist,dist_horizon,dist_verti);
+            forcaResultante[0] += forca[0];
+            forcaResultante[1] += forca[1];
+
+            //Calcular com próximo da lista
+            unidadeElementoFracoContra = unidadeElementoFracoContra->prox;
+        }
+
+            //Há menos um leitor. Se for o último, libere as listas
+        pthread_mutex_lock(&mutexNumLeitores);
+        numLeitores--;
+        if(numLeitores == 0) pthread_mutex_unlock(&mutexListas);
+        pthread_mutex_unlock(&mutexNumLeitores);
+
+            //Calculando aceleração e atualizando velocidade e posição
+        calcula_aceleracao(aceleracao,forcaResultante,MASSA);
+        calcula_velocidade(velocidadeAtualElemento,aceleracao,dt);
+        calcula_posicao(posicaoAtualElemento,velocidadeAtualElemento,dt);
+
+            //Atualizando a posição na unidade respectiva
+        elemento.respectivaUnidade->dado.x = posicaoAtualElemento[0];
+        elemento.respectivaUnidade->dado.y = posicaoAtualElemento[1];
+
+        //----------------------Escreve instância do ponto no arquivo----------------------//
+
+        
+    }
 }
